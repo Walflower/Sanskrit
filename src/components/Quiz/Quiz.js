f48ba622ec0dbd9e05112fc23edaaab5faa4cf54
Nyajal Wal <nyajal.w@gmail.com>
2024-02-21 18:36:02 -0500

    rendered poses on quiz page

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,37 +1,49 @@
 import "./Quiz.scss";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import Logo from "../Logo/Logo";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 
 function Quiz() {
-  const { BASE_URL } = process.env;
-  const [yogaPose, setYogaPose] = useState({});
+  const BASE_URL = process.env.REACT_APP_API_BASE_PATH;
+  const [yogaPose, setYogaPose] = useState([]);
+  const [randomSelections, setRandomSelections] = useState([]);
+  const [currentPose, setCurrentPose] = useState(0);
 
+  //fetching all poses from api
   const getPose = async () => {
     try {
       const poseData = await axios.get(`${BASE_URL}/poses`);
       setYogaPose(poseData.data);
+      console.log(poseData.data);
     } catch (error) {
       console.error("Error fetching pose data:", error);
     }
   };
 
+  useEffect(() => {
+    getPose();
+  }, []);
+
+  const goForward = () => {
+    setCurrentPose((clickedPose) => {
+      return clickedPose + 1;
+    });
+  };
+
+  if (!yogaPose[0]) {
+    return <p>loading...</p>;
+  }
+
   return (
     <>
-      <div>
-        <div>blank</div>
-        <img></img>
-        <p>english name</p>
-        <p>description</p>
+      <h1>Input Sanskrit name of pose</h1>
 
-        <div>
-          <div>s</div>
-          <div>s</div>
-          <div>s</div>
-          <div>s</div>
-          <div>s</div>
-          <div>s</div>
-        </div>
+      <div>
+        <p>Pose: {yogaPose[currentPose]?.pose}</p>
+        <p>Sanskrit Name: {yogaPose[currentPose]?.sanskrit_name}</p>
+        <img src={yogaPose[currentPose]?.photo} alt="yoga pose" />
+        <p>Description: {yogaPose[currentPose]?.description}</p>
+
+        <button onClick={goForward}>Next Question</button>
       </div>
     </>
   );
