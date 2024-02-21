0bac52ad7ea700843c05a1b507f572732cd0ed06
Nyajal Wal <nyajal.w@gmail.com>
2024-02-20 20:35:18 -0500

    variables

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,24 @@
+import "./Welcome.scss";
+
+function Welcome() {
+  return (
+    <>
+      <header>LOGO</header>
+      <h1>Welcome</h1>
+
+      <form>
+        <h2>Login</h2>
+
+        <label>Email:</label>
+        <input></input>
+
+        <label>Password:</label>
+        <input></input>
+      </form>
+
+      <button>REGISTER</button>
+    </>
+  );
+}
+
+export default Welcome;
