14dbc0b638f734a00011c11d59ac33b9b6915d78
Nyajal Wal <nyajal.w@gmail.com>
2024-02-25 14:12:55 -0500

    styling for register component comeplete. added green cta mixin

--- src/components/Begin/BeginQuiz.js
+++ src/components/Begin/BeginQuiz.js
@@ -3,13 +3,11 @@ import { Link } from "react-router-dom";
 
 function BeginQuiz() {
   return (
-    <main className="large">
-      <div className="begin">
-        <h1 className="begin__title">Begin</h1>
-        <Link to="/quiz">
-          <button className="begin__button">START</button>
-        </Link>
-      </div>
+    <main className="begin">
+      <h1 className="begin__title">Begin</h1>
+      <Link to="/quiz">
+        <button className="begin__button">START</button>
+      </Link>
     </main>
   );
 }
