47a0ccea3c78097f727e86b1ce758dd20dcc925f
Nyajal Wal <nyajal.w@gmail.com>
2024-02-29 13:35:30 -0500

    adding scrolling images

--- src/components/Sound/sound.js
+++ src/components/Sound/sound.js
@@ -15,10 +15,27 @@ function Sound() {
     }
     setIsPlaying(!isPlaying);
   };
+
+  // Path to the image for play and pause states
+  const playImage = "/path/to/play-image.png";
+  const pauseImage = "/path/to/pause-image.png";
+
+  // Dynamically determine which image to display based on the isPlaying state
+  const imageSrc = isPlaying ? pauseImage : playImage;
+
   return (
-    <button onClick={toggleSound}>
-      {isPlaying ? "Pause Sound" : "Play Sound"}
-    </button>
+    <>
+      <button onClick={toggleSound}>
+        {isPlaying ? "Pause Sound" : "Play Sound"}
+      </button>
+
+      {/* <img
+src={imageSrc}
+alt={isPlaying ? "Pause Sound" : "Play Sound"}
+onClick={toggleSound}
+style={{ cursor: "pointer" }} // Style to show pointer cursor on hover
+/> */}
+    </>
   );
 }
 
